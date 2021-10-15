1ea19671fcdfccc769610100a09c3a6864e0df1d
EitaroNishimura <e.nishimura.0824@gmail.com>
2021-10-15 20:49:17 +0900

    メモアプリペアワーク

--- 04_ゼミ/week3/main.js
+++ 04_ゼミ/week3/main.js
@@ -2,9 +2,23 @@ const input = document.getElementById("memo-input")
 const addButton = document.getElementById("add-button")
 const memoContainer = document.getElementById("memo-container")
 let memos = []
+if (localStorage.memos) {
+  // ローカルストレージのmemosキーからJSONを読み込む。
+  const memosJson = localStorage.memos
+  // JSON.parseで配列にして、memosを更新する。
+  memos = JSON.parse(memosJson)
+
+  // ページ読み込み時に、配列の内容をブラウザに反映する。
+  for (let i = 0; i < memos.length; i++) {
+    const div = document.createElement("div")
+    div.textContent = memos[i]
+    memoContainer.append(div)
+  }
+}
 
 addButton.onclick = function() {
   memos.push(input.value)
+  localStorage.memos = JSON.stringify(memos)
   console.log(memos)
   memoContainer.innerHTML = ""
   for (let i = 0; i < memos.length; i++) {
